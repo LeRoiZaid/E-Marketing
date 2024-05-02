47264b427f71ebba17be75da0eb10b35546c39f0
LeRoiZaid <98708741+LeRoiZaid@users.noreply.github.com>
2024-05-02 04:03:26 +0300

    firstRegistrationController

---
+++ e_markting/lib/features/auth/Registration/controller/cubit/registration_cubit.dart
@@ -0,0 +1,38 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+
+import 'package:flutter/material.dart';
+
+part 'registration_state.dart';
+
+
+class RegistrationCubit extends Cubit<RegistrationState> {
+  RegistrationCubit() : super(RegistrationInitial());
+  TextEditingController UserNameController = TextEditingController();
+  TextEditingController EmailController = TextEditingController();
+  TextEditingController PasswordController = TextEditingController();
+  TextEditingController ConfirmPassController = TextEditingController();
+    GlobalKey<FormState> formKey = GlobalKey<FormState>();
+    void onPressedConfirmButton(BuildContext context) {
+    if (formKey.currentState!.validate()) {
+   // ignore: prefer_typing_uninitialized_variables
+  
+   Navigator.pushNamed(context, 'login',
+    arguments: [
+    UserNameController.text,
+      ]);
+  log( UserNameController.text);
+  log(  EmailController .text);
+  log( PasswordController.text); 
+  log( ConfirmPassController.text); 
+    
+    
+    
+    } else {
+      log('invalid inputs');
+    }
+  }
+}
--- e_markting/lib/main.dart
+++ e_markting/lib/main.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: unnecessary_const
 
+// ignore: depend_on_referenced_packages
+import 'package:device_preview/device_preview.dart';
 import 'package:e_markting/features/auth/onboarding/view/page/onboarding_page.dart';
 import 'package:flutter/material.dart';
 import 'package:e_markting/features/auth/login/view/page/login_page.dart';
@@ -10,35 +12,63 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-/*  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-  bool onBording = sharedPreferences.getBool('OnBoardingPage') ?? false;*/
+
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  bool onBording = sharedPreferences.getBool('OnBoardingPage') ?? false;
 
  
 
 MaterialApp materialApp = MaterialApp(
-  //home:  onBording ? const RegistrationPage() : const OnBoardingPage()
-  home:OnBoardingPage(),
+   builder:DevicePreview.appBuilder,
+    useInheritedMediaQuery: true,
+    onGenerateRoute: MyRoutes.onGenerateRoute,
+    onGenerateInitialRoutes: (_) => MyRoutes.initRoutes,
 );
 
     //VerficationPage()
     //RegistrationPage()
     //ChangePassPage()
     //LoginPage()
-  runApp(materialApp);
+  runApp(  DevicePreview(
+      enabled: false,
+      builder: (context) => materialApp, // Wrap your app
+    ),);
 }
 
-/*class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyRoutes {
+  static List<Route> initRoutes = [
+    MaterialPageRoute<dynamic>(
+      builder: (BuildContext context) => const OnBoardingPage(),
+    ),
+  ];
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case 'login':
+        final List data = settings.arguments as List;
+        return MaterialPageRoute<dynamic>(
+          builder: (BuildContext context) => LoginPage(),
+        );
+      case 'registration':
+        final List data = settings.arguments as List;
+        return MaterialPageRoute<dynamic>(
+          builder: (BuildContext context) =>RegistrationPage(),
+        );
+      case 'verfiction':
+        final List data = settings.arguments as List;
+        return MaterialPageRoute<dynamic>(
+          builder: (BuildContext context) => VerficationPage(),
+        );
+      case 'change password':
+        return MaterialPageRoute<dynamic>(
+          builder: (BuildContext context) => const ChangePassPage(),
+        );
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+      default:
+        return MaterialPageRoute<dynamic>(
+          builder: (BuildContext context) => const OnBoardingPage(),
+        );
+    }
   }
-}*/
+}
